1ffda951c07ed64e44d5fe88de53419baacb3731
nimishamishra13 <nimishanm13@gmail.com>
2026-08-07 20:45:51 +0530

    Final proj

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from database import models
 
 # Module 1 (Leads)
 from modules.module1_leads import router as leads_router
+from modules.auth import router as auth_router
 
 # Module 2 (Company Intelligence + Lead Scoring)
-#from modules.module2_intelligence import router as intelligence_router
 
+# Module 3 (AI Outreach Generation)
+from modules.module3_outreach import router as outreach_router
+
+# Module 4 (AI Lead Scoring + Recommendation)
+from modules.module4_scoring import router as scoring_router
+
+from modules.module5_conversation import router as conversation_router
 app = FastAPI()
 
 # Create tables
@@ -15,6 +22,14 @@ Base.metadata.create_all(bind=engine)
 
 # Include Module 1
 app.include_router(leads_router)
+app.include_router(auth_router)
 
-# Include Module 2 (VERY IMPORTANT for fixing 404)
-#app.include_router(intelligence_router)
+# Include Module 2 
+
+# Include Module 3 
+app.include_router(outreach_router)
+
+# Include Module 4 
+app.include_router(scoring_router)
+
+app.include_router(conversation_router)
