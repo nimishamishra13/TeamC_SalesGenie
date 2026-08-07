15dda562cbfa01a019f52c2c917201e2decee604
nimishamishra13 <nimishanm13@gmail.com>
2026-08-07 20:36:23 +0530

    Final proj

--- modules/module2_intelligence.py
+++ modules/module2_intelligence.py
@@ -1,5 +1,5 @@
 from ai.ai_analysis import analyze_lead
-
+from ai.tech_stack import detect_tech_stack
 
 def get_status(score):
     if score >= 90:
@@ -10,25 +10,33 @@ def get_status(score):
 
 
 def analyze_and_score_lead(data: dict):
+    combined_text = f"""
+        {data.get('website')}
+        {data.get('notes')}
+        """
+
+    tech_stack = detect_tech_stack(combined_text)
     prompt = f"""
-Company: {data.get('company')}
-Industry: {data.get('industry')}
-Location: {data.get('location')}
-Website: {data.get('website')}
+        Company: {data.get('company')}
+        Industry: {data.get('industry')}
+        Location: {data.get('location')}
+        Website: {data.get('website')}
 
-Primary Contact: {data.get('contact')}
-Designation: {data.get('designation')}
+        Detected Technology Stack:
+        {", ".join(tech_stack) if tech_stack else "Not detected"}
 
-Additional Notes:
-{data.get('notes')}
+        Primary Contact: {data.get('contact')}
+        Designation: {data.get('designation')}
 
-Evaluate the lead according to the scoring criteria.
-"""
+        Additional Notes:
+        {data.get('notes')}
 
+        Evaluate the lead according to the scoring criteria.
+        """
     result = analyze_lead(prompt)
-
+    
     return {
-        "analysis": result,
-        "score": result["lead_score"],
-        "status": get_status(result["lead_score"])
-    }
+            "analysis": result,
+            "score": result["lead_score"],
+            "status": get_status(result["lead_score"])
+        }
